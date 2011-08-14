3c8c94412cede963ad7bfdb30242ed640d8eee16
David Golden <dagolden@cpan.org>
2011-08-13 20:44:36 -0400

    initial import of App::mymeta_requires

---
+++ lib/App/mymeta_requires.pm
@@ -0,0 +1,40 @@
+use 5.008001;
+use strict;
+use warnings;
+
+package App::mymeta_requires;
+# VERSION
+
+# Dependencies
+use autodie 2.00;
+
+1;
+
+# ABSTRACT: No abstract given for App::mymeta_requires
+
+=for Pod::Coverage method_names_here
+
+=begin wikidoc
+
+= SYNOPSIS
+
+  use App::mymeta_requires;
+
+= DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+= USAGE
+
+Good luck!
+
+= SEE ALSO
+
+Maybe other modules do related things.
+
+=end wikidoc
+
+=cut
+
+# vim: ts=2 sts=2 sw=2 et:
